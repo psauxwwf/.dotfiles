9df13611400119e76089272de3b9e3caa56b5665
devil666face <in@d6f.ru>
2026-02-19 11:34:50 +0300

    chore(env): sync skills and Cyrillic bindings

--- dist/gnome/bind.sh
+++ dist/gnome/bind.sh
@@ -26,7 +26,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "[]"
 gsettings set org.gnome.mutter.keybindings toggle-tiled-left "[]"
 gsettings set org.gnome.mutter.keybindings toggle-tiled-right "[]"
 
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>grave', '<Super>Cyrillic_shorti']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>grave', '<Super>ё']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>2']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>3']"
@@ -38,7 +38,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>8
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>9']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-11 "['<Super>0']"
 
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>grave', '<Super><Shift>Cyrillic_shorti']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>grave', '<Super><Shift>ё']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Super><Shift>1']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Super><Shift>2']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Super><Shift>3']"
@@ -50,7 +50,7 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Super><Sh
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>9']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-11 "['<Super><Shift>0']"
 
-gsettings set org.gnome.desktop.wm.keybindings close "['<Super>Cyrillic_shorti', '<Super>q']"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>й', '<Super>q']"
 
 gsettings set org.gnome.shell.keybindings toggle-application-view "['<Super>d']"
 gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>a']"
