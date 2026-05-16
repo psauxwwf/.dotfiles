211cfdd036d49835df299fa0678eba31a6863680
psauxwwf <psauxwwf@d6f.ru>
2026-05-16 17:58:36 +0300

    fix: mise task

---
+++ .config/zellij/README.md
@@ -0,0 +1,5 @@
+```
+rm -rf zjframes.wasm zjstatus.wasm
+wget https://github.com/dj95/zjstatus/releases/latest/download/zjframes.wasm
+wget https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm
+```
---
+++ dist/gnome/theme.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+wget https://github.com/dracula/gtk/archive/master.zip
+unzip master.zip
+mv gtk-master/ Dracula
+sudo mv Dracula /usr/share/themes/
+rm master.zip
+
+wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+unzip Dracula.zip
+sudo mv Dracula /usr/share/icons/
+rm Dracula.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+mkdir JetBrainsMono
+unzip JetBrainsMono.zip -d JetBrainsMono
+sudo mv JetBrainsMono /usr/share/fonts
+fc-cache -fv
+rm JetBrainsMono.zip
+
+wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
+tar -xf Bibata-Modern-Ice.tar.xz
+sudo mv Bibata-Modern-Ice /usr/share/icons
+rm Bibata-Modern-Ice.tar.xz
+
+gsettings set org.gnome.desktop.wm.preferences button-layout ':'
+
+wget https://github.com/hardpixel/unite-shell/releases/download/v84/unite-v84.zip
+gnome-extensions install --force unite-v84.zip
