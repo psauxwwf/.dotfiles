3a0a9d34f0a8420c5b5fd4a91e5617f6bd0b5345
psauxwwf <psauxwwf@d6f.ru>
2026-05-17 19:54:08 +0300

    feat: add lutris to flatpack

--- dist/flatpak.sh
+++ dist/flatpak.sh
@@ -2,3 +2,5 @@
 flatpak install --system be.alexandervanhee.gradia -y
 flatpak install --system io.github.realmazharhussain.GdmSettings -y
 flatpak install --system io.github.swordpuffin.rewaita -y
+# flatpak install --system net.lutris.Lutris -y
+# flatpak install --system com.github.tchx84.Flatseal -y
